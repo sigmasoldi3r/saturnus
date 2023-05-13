003aab46a530119e53e69d1372c696d0c5b640b3
sigmasoldi3r <pablobc.1995@gmail.com>
2023-05-14 01:30:38 +0200

    Added basic CLI options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+};
+
+use clap::Parser;
+use code::Visitor;
+
 #[cfg(test)]
 #[macro_use]
 extern crate spectral;
@@ -9,6 +18,46 @@ mod parser;
 #[cfg(test)]
 mod tests;
 
+#[derive(Parser)]
+struct Args {
+    #[arg(
+        short,
+        long,
+        help = "An optional output file, if not provided the extension is replaced by .lua"
+    )]
+    output: Option<String>,
+    #[arg(short, long, help = "The input file")]
+    input: String,
+    #[arg(
+        short = 'p',
+        long = "print",
+        help = "Prints the compilation result to the standard output"
+    )]
+    pipe: bool,
+}
+
+fn get_default_output(str: &String) -> String {
+    Path::new(str)
+        .with_extension("lua")
+        .to_str()
+        .unwrap()
+        .to_string()
+}
+
 fn main() {
-    todo!("Replace with actual code I/O")
+    let args = Args::parse();
+    let in_path = args.input;
+    let out_path = args.output.unwrap_or(get_default_output(&in_path));
+    let mut in_file = File::open(in_path).unwrap();
+    let mut input = String::new();
+    in_file.read_to_string(&mut input).unwrap();
+    let output = lua::LuaEmitter
+        .visit_script(
+            code::Builder::new("  "),
+            &parser::Script::parse(input).unwrap(),
+        )
+        .unwrap()
+        .collect();
+    let mut out_file = File::create(out_path).unwrap();
+    out_file.write_all(output.as_bytes()).unwrap();
 }
