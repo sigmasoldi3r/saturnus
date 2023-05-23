90b0793860006b0f2136cbbc1184b89bd4209d16
sigmasoldi3r <pablobc.1995@gmail.com>
2023-05-23 13:50:12 +0200

    Added runtime options to saturnus

--- src/main.rs
+++ src/main.rs
@@ -23,14 +23,16 @@ struct Args {
         help = "An optional output file, if not provided the extension is replaced by .lua"
     )]
     output: Option<String>,
-    #[arg(short, long, help = "The input file")]
+    #[arg(help = "The input file to evaluate and/or compile")]
     input: String,
+    #[arg(short, long, help = "Compiles the Saturnus script")]
+    compile: bool,
     #[arg(
         short = 'p',
         long = "print",
         help = "Prints the compilation result to the standard output"
     )]
-    pipe: bool,
+    print: bool,
     #[arg(
         long,
         help = "If used, the compilation output emits tab characters. Ignores indentation parameter"
@@ -41,7 +43,7 @@ struct Args {
         long,
         help = "The amount of space characters to use in each tab"
     )]
-    indentation: usize,
+    indent: usize,
 }
 
 fn get_default_output(str: &Path) -> String {
@@ -58,13 +60,12 @@ fn main() {
     let indent = if args.use_tabs {
         "\t".to_string()
     } else {
-        " ".repeat(args.indentation)
+        " ".repeat(args.indent)
     };
     use std::fs::read_to_string;
 
     // Read input files
     let in_path = Path::new(&args.input);
-    println!("Compiling {:?}...", in_path);
     let out_path = args.output.unwrap_or(get_default_output(in_path));
     let input = read_to_string(in_path).unwrap();
 
@@ -80,45 +81,68 @@ fn main() {
                 )
                 .unwrap()
                 .collect();
-            let mut out_file = File::create(out_path).unwrap();
-            out_file.write_all(output.as_bytes()).unwrap();
+            if args.compile {
+                println!("Compiling {:?}...", in_path);
+                if args.print {
+                    println!("\n------\n\n");
+                    std::io::stdout().write_all(output.as_bytes()).unwrap();
+                } else {
+                    let mut out_file = File::create(out_path).unwrap();
+                    out_file.write_all(output.as_bytes()).unwrap();
+                }
+            } else {
+                let rt = rlua::Lua::new();
+                rt.context(move |ctx| -> rlua::Result<()> {
+                    ctx.load(&output).eval()?;
+                    Ok(())
+                })
+                .unwrap();
+            }
         }
         Err(err) => {
-            eprintln!("Failed to parse {} file!", args.input);
-            let line = err.location.line;
-            let col = err.location.column;
-            let ep = err
-                .expected
-                .tokens()
-                .map(|x| String::from(x))
-                .reduce(|a, b| format!("{}, {}", a, b));
-            eprintln!("At {}:{}:{}", args.input, line, col);
-            let lines = input.lines();
-            let mut i = 0_usize;
-            let mut pos = 0_usize;
-            for line_str in lines {
-                pos += 1;
-                if pos >= line - 3 && pos < line + 5 {
-                    let n = pos.to_string();
-                    let numeric = format!("{:>4}", n);
-                    let numeric = style(numeric).blue();
-                    let divider = style("|").green().bold();
-                    eprintln!("{} {} {}", numeric, divider, line_str);
-                    if line == pos {
-                        let ted = line_str.len() - col;
-                        let premark = style("     |").red().bold();
-                        let spanner = format!(" {:2$}{:^<3$}", " ", "^", col - 2, ted);
-                        let spanner = style(spanner).red();
-                        let here = style("here").red();
-                        eprintln!("{} {} {here}", premark, spanner);
-                    }
-                    i += 1;
-                }
-                if i > 5 {
-                    break;
-                }
-            }
+            report_error(args.input.clone(), input.clone(), err);
             panic!("Compilation failed");
+        }
+    }
+}
+
+fn report_error(file: String, input: String, err: peg::error::ParseError<peg::str::LineCol>) {
+    eprintln!("Failed to parse {} file!", file);
+    let line = err.location.line;
+    let col = err.location.column;
+    let ep = err
+        .expected
+        .tokens()
+        .map(|x| String::from(x))
+        .reduce(|a, b| format!("{}, {}", a, b));
+    eprintln!("At {}:{}:{}", file, line, col);
+    if let Some(ep) = ep {
+        eprintln!("Expected: one of {}", ep);
+    }
+    let lines = input.lines();
+    let mut i = 0_usize;
+    let mut pos = 0_usize;
+    for line_str in lines {
+        pos += 1;
+        let to_sub = 3.min(line);
+        if pos >= line - to_sub && pos < line + 5 {
+            let n = pos.to_string();
+            let numeric = format!("{:>4}", n);
+            let numeric = style(numeric).blue();
+            let divider = style("|").green().bold();
+            eprintln!("{} {} {}", numeric, divider, line_str);
+            if line == pos {
+                let ted = line_str.len() - col;
+                let premark = style("     |").red().bold();
+                let spanner = format!(" {:2$}{:^<3$}", " ", "^", col - 2, ted);
+                let spanner = style(spanner).red();
+                let here = style("here").red();
+                eprintln!("{} {} {here}", premark, spanner);
+            }
+            i += 1;
+        }
+        if i > 5 {
+            break;
         }
     }
 }
